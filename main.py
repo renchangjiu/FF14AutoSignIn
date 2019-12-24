ce2b79f70c2c4eaa12bd6e41e5b1cca7a52813a8
su <1359581862@qq.com>
2019-12-24 16:42:22 +0800

    commit

--- main.py
+++ main.py
@@ -202,6 +202,17 @@ def step8():
     logger.info("当前积分为: %d" % jifen)
 
 
+def step9():
+    url = "http://act.ff.sdo.com/20191225Christmas/Server/User.ashx"
+    params = {
+        "method": "signin",
+        "i": "0.5218470090028671"
+    }
+    r = requests.post(url, params=params, cookies=cookies)
+    obj = json.loads(r.text)
+    print(obj)
+
+
 def main():
     ticket = step1()
     if ticket == "":
@@ -215,6 +226,7 @@ def main():
     step6(role)
     step7()
     step8()
+    step9()
     time.sleep(5)
 
 
